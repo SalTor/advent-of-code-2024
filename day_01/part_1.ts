b0f0e143b1c070866f6c3d14394c7d9cfc8fa346
Sal Torcivia <storcivia@fastgrowingtrees.com>
2024-12-03 19:49:39 -0500

    rename to make room for multiple parts

--- day_01/part_1.ts
+++ day_01/part_1.ts
@@ -4,16 +4,14 @@ import fs from "fs";
   const inputA = [3, 4, 2, 1, 3, 3];
   const inputB = [4, 3, 5, 3, 9, 3];
 
-  const expectedResult = 11;
-
   console.info("Example run:", distanceGet(inputA, inputB));
 }
 
 {
-  const inputRaw = fs.readFileSync("input.txt", "utf8");
+  const inputRaw = fs.readFileSync("input_part_1.txt", "utf8");
 
-  let inputA: Array<number> = [];
-  let inputB: Array<number> = [];
+  const inputA: Array<number> = [];
+  const inputB: Array<number> = [];
 
   for (const row of inputRaw.split("\n")) {
     let [num1, num2] = row.split("   ");
@@ -22,10 +20,7 @@ import fs from "fs";
     inputB.push(parseInt(num2));
   }
 
-  inputA = [...inputA].sort((a, b) => a - b);
-  inputB = [...inputB].sort((a, b) => a - b);
-
-  console.info("Real run", distanceGet(inputA, inputB));
+  console.info("Real run:", distanceGet(inputA, inputB));
 }
 
 function distanceGet(inputA: Array<number>, inputB: Array<number>): number {
