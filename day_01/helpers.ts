068f05d598970f0c37e87e9db46000330bb0696f
Sal Torcivia <storcivia@fastgrowingtrees.com>
2024-12-05 21:41:36 -0500

    complete day 1 part 2

--- day_01/helpers.ts
+++ day_01/helpers.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-export function getInput(fileName: string) {
+export function getInput(fileName: string): Array<string> {
   const inputRaw = fs.readFileSync(fileName, "utf8");
   return inputRaw.split("\n").filter(Boolean);
 }
---
+++ day_01/part_2.ts
@@ -0,0 +1,41 @@
+import fs from "fs";
+import { getInput } from "./helpers";
+
+{
+  const listA = [3, 4, 2, 1, 3, 3];
+  const listB = [4, 3, 5, 3, 9, 3];
+
+  console.info("Example run:", similarityGet(listA, listB));
+}
+
+{
+  const listA: Array<number> = [];
+  const listB: Array<number> = [];
+
+  const input = getInput("input_part_1.txt");
+
+  for (const row of input) {
+    let [num1, num2] = row.split("   ");
+    if (!num1 || !num2) continue;
+    listA.push(parseInt(num1));
+    listB.push(parseInt(num2));
+  }
+
+  console.info("Real run:", similarityGet(listA, listB));
+}
+
+function similarityGet(listA: Array<number>, listB: Array<number>) {
+  const list_b_repeats = new Map();
+  for (const num of listB) {
+    const currentCount = list_b_repeats.get(num) || 0;
+    list_b_repeats.set(num, currentCount + 1);
+  }
+
+  let result = 0;
+  for (const num of listA) {
+    const numRepeatCount = list_b_repeats.get(num) || 0;
+    result += num * numRepeatCount;
+  }
+
+  return result;
+}
