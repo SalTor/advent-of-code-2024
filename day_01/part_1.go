65f463a0047dc6d8ef143795a047abc1e6c0bc8d
Sal Torcivia <storcivia@fastgrowingtrees.com>
2024-12-13 00:30:55 -0500

    fix go day1 part1

--- day_01/part_1.go
+++ day_01/part_1.go
@@ -1,34 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"testing"
+	"sort"
 )
 
-func main() {
-	fmt.Println("Hello, World!")
+func Abs(num int) int {
+	if num > 0 {
+		return num
+	}
+	return -num
 }
 
 func DistanceGet(inputA, inputB []int) int {
 	distance := 0
 
-	for i := 0; i < len(inputA); i++ {
-		distance = distance + (inputA[i] - inputB[i])
+	sort.Slice(inputA, func(i, j int) bool {
+		return inputA[i] < inputA[j]
+	})
+
+	sort.Slice(inputB, func(i, j int) bool {
+		return inputB[i] < inputB[j]
+	})
+
+	for index, _ := range inputA {
+		num_a := inputA[index]
+		num_b := inputB[index]
+		distance = distance + Abs(num_a-num_b)
 	}
 
 	return distance
-}
-
-func TestDistanceGet(t *testing.T) {
-	inputA := []int{3, 4, 2, 1, 3, 3}
-	inputB := []int{4, 3, 5, 3, 9, 3}
-	expected := 11
-
-	subject := DistanceGet(inputA, inputB)
-
-	if subject != expected {
-		t.Errorf("Invalid answer %d", subject)
-	} else {
-		fmt.Println("Success")
-	}
 }
